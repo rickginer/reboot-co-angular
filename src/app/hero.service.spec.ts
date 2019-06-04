21577c004e583d6b3dcc9f895d885f677320ca6b
Rick Giner <rickginer@gmail.com>
2019-06-04 16:32:23 +1000

    Hero Service getHeroes positive scenario test

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,9 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
-describe('HeroService', () => {
+const mockData = [
+  { id: 1, name: 'Hulk' },
+  { id: 2, name: 'Thor'},
+  { id: 3, name: 'Iron Man'}
+] as Hero[];
+
+fdescribe('HeroService', () => {
+
+  let service;
+  let httpTestingController: HttpTestingController;
+
+  let mockHeroes;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -11,12 +24,37 @@ describe('HeroService', () => {
       ],
       providers: [HeroService]
     });
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  beforeEach(inject([HeroService], s => {
+    service = s;
+  }));
+
+  beforeEach(() => {
+    mockHeroes = [...mockData];
   });
 
   it('should be created', () => {
     const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
   });
-  
+
+  describe('getHeroes', () => {
+
+    it('should return mock heroes', () => {
+      service.getHeroes().subscribe(
+        heroes => expect(heroes.length).toEqual(mockHeroes.length),
+        fail
+      );
+      // Receive GET request
+      const req = httpTestingController.expectOne(service.heroesUrl);
+      expect(req.request.method).toEqual('GET');
+      // Respond with the mock heroes
+      req.flush(mockHeroes);
+    });
+
+  });
+
 });
 
