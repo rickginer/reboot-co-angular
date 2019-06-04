d5b94d9b70bd62bf8c2215feee45289cfae08075
Rick Giner <rickginer@gmail.com>
2019-06-04 16:08:40 +1000

    Hero service tests. All tests now passing

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [HeroService]
+    });
+  });
 
   it('should be created', () => {
     const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
   });
+  
 });
+
