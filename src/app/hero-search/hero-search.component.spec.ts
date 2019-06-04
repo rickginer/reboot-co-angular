dff007eac87b7d6bb8613056229ad0a52cd81b85
Rick Giner <rickginer@gmail.com>
2019-06-04 15:27:47 +1000

    tests for hero-search component

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeroSearchComponent } from './hero-search.component';
+import { HeroService } from '../hero.service';
+import { MockHeroService } from '../hero.service.mock';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
@@ -8,7 +10,11 @@ describe('HeroSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ]
+      declarations: [ HeroSearchComponent ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        { provide: HeroService, useClass: MockHeroService },
+      ]
     })
     .compileComponents();
   }));
