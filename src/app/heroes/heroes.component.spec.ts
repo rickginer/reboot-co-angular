a1525f74b6306208ee1f29c490041bb32fdde97f
Rick Giner <rickginer@gmail.com>
2019-06-04 15:24:58 +1000

    heroes component tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeroesComponent } from './heroes.component';
+import { HeroService } from '../hero.service';
+import { MockHeroService } from '../hero.service.mock';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -8,7 +10,11 @@ describe('HeroesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [ HeroesComponent ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        { provide: HeroService, useClass: MockHeroService },
+      ]
     })
     .compileComponents();
   }));
